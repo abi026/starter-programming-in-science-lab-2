2287a36a4d3ff1e6713dac47835ec0fb61d1f96b
abi026 <6307479@edu.vaniercollege.qc.ca>
2025-09-05 14:53:53 -0400

    Update Lab2.py

--- Lab2.py
+++ Lab2.py
@@ -1,12 +1,17 @@
-# Function 1: Calculate the height of the ball after time t
 # This function should take the initial height h0 and time t as inputs, and return the height at time t.
 # Round up to one decimal point
 def calculate_height(h0, t):
     # TODO: Implement this function
     pass  # Replace with your code
+    g= 9.8
+    h= h0 - 0.5 * g * t**2
+    return round(height, 2)
 
 # Function 2: Calculate the distance traveled by the car
 # This function should take the time t as input and return the distance traveled by the car.
 def calculate_car_distance(t):
     # TODO: Implement this function
     pass  # Replace with your code
+    speed= 20
+    distance = speed * t
+    return distance
