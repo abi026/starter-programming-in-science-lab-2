3770927e2f7cce77220cbb4171641abd9430349d
abi026 <6307479@edu.vaniercollege.qc.ca>
2025-09-05 14:55:09 -0400

    Update Lab2.py

--- Lab2.py
+++ Lab2.py
@@ -4,9 +4,16 @@
 def calculate_height(h0, t):
     # TODO: Implement this function
     pass  # Replace with your code
+    g= 9.8
+    h= h0 - 0.5 * g * t**2
+    return round(height, 2)
+
 
 # Function 2: Calculate the distance traveled by the car
 # This function should take the time t as input and return the distance traveled by the car.
 def calculate_car_distance(t):
     # TODO: Implement this function
     pass  # Replace with your code
+    speed= 20
+    distance = speed * t
+    return distance
